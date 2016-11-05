ece08365f0b89a56ca98a070e9cbdb56ad9be5bc
Cla Tschenett <design@clatschenett.com>
2016-11-05 19:22:59 +0100

    Constructor added

--- src/Punkt.java
+++ src/Punkt.java
@@ -1,2 +1,41 @@
+import Prog1Tools.IOTools;
+
 public class Punkt {
+
+    private double x;
+    private double y;
+
+
+// KONSTRUKTOREN
+    public Punkt(double x,double y) {
+        this.getX();
+        this.getY();
+    }
+
+// GETER
+
+    public double getX() {
+        return x;
+    }
+
+
+    public double getY() {
+        return y;
+    }
+
+//READ
+
+    public void read() {
+        x = IOTools.readDouble();
+        y = IOTools.readDouble();
+    }
+
+    @Override
+    public String toString() {
+         return "(" + x + "," + y + ")";
+    }
+
+
 }
+
+
--- src/TestStrecke.java
+++ src/TestStrecke.java
@@ -1,2 +1,16 @@
 public class TestStrecke {
+    public static void main(String[] args) {
+        Punkt ursprung = new Punkt(0.0,0.1);
+        Punkt endpunkt = new Punkt(4.0,3.0);
+
+        /*Strecke s = new Strecke(ursprung,endpunkt);
+        System.out.println("Die Laenge der Strecke " + s + " betraegt " + s.getLaenge() + ".");
+        System.out.println();
+        System.out.println("Strecke s Eingeben: ");
+        s.read();
+        System.out.println();
+        System.out.println("Die Laenge der Strecke " + s + " betraegt " + s.getLaenge() + ".");
+        */
+    }
+
 }
